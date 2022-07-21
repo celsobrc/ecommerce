c150e282537b4232f717a47a09fd9f989955005f
Chelso <celsobrc@gmail.com>
2022-07-21 19:11:14 -0300

    Initial backend commit

---
+++ backend/src/main/java/com/chelso/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.chelso.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
